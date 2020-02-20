c13bdb12f33b4198feeb6869a8fd603303417868
Steven Prindle <flankbusiness@gmail.com>
2020-02-20 07:33:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,5 +22,5 @@ def assign_rooms(attendees)
 end
 
 def printer(arr_attendees)
-  batch_badge_creator(arr_attendees).each {|phrase
+  batch_badge_creator(arr_attendees).each {|phrase| puts phrase 
 end
